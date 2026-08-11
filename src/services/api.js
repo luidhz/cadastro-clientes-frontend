521259ae5fdd874c80e64d12900a0d0a81abc7b6
Brenno <ubrennz@gmail.comm>
2026-08-11 15:44:21 -0300

    refactor: URL produção

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import { SECTIONS } from '../constants/sections'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080'
+const API_URL = import.meta.env.VITE_API_URL || 'http://cadastro-clientes-spring-m81bl0-cb8c34-144-126-149-173.sslip.io'
 
 function getToken() {
   const salvo = localStorage.getItem('usuarioLogado')
